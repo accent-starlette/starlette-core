a6e3a34edb9c794f71cbfe8913cf04290e8dabf7
Stuart George <stuart@accentdesign.co.uk>
2019-05-14 17:14:15 +0100

    Added an email type

--- starlette_core/typesystem.py
+++ starlette_core/typesystem.py
@@ -3,7 +3,28 @@ import typesystem
 from typesystem.unique import Uniqueness
 
 
+class Email(typesystem.String):
+    """ A field that validates an email """
+
+    custom_errors = {
+        'pattern': 'Must be a valid email.'
+    }
+
+    def __init__(self, **kwargs: typing.Any) -> None:
+        kwargs.setdefault('pattern', r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)')
+        kwargs.setdefault('format', 'email')
+        self.errors.update(self.custom_errors)
+        super().__init__(**kwargs)
+
+
 class IntegerChoice(typesystem.Choice):
+    """ A choice field that allows its choices to be integers
+
+    Example:
+
+        choices = [(1, "One"), (2, "Two")]
+    """
+
     errors = {
         "null": "May not be null.",
         "required": "This field is required.",
