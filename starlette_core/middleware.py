fe2dfa8b18e5e40d9ddb1b7e03b786d9222635e8
Stuart George <stuart@accentdesign.co.uk>
2019-07-16 11:20:53 +0100

    moved session import to aid tests

--- starlette_core/middleware.py
+++ starlette_core/middleware.py
@@ -1,17 +1,22 @@
-from starlette.middleware.base import BaseHTTPMiddleware
-
-from .database import Session
+from starlette.types import Scope, Receive, Send
 
 
-class DatabaseMiddleware(BaseHTTPMiddleware):
+class DatabaseMiddleware:
     """
     Remove the current db session so it doesnt live between
     requests. This should be added after all middleware
     that requires db access have been added.
     """
 
-    async def dispatch(self, request, call_next):
-        response = await call_next(request)
+    def __init__(self, app) -> None:
+        self.app = app
+
+    def cleanup_session(self):
+        from .database import Session
+
         if Session.registry.has():
             Session.remove()
-        return response
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        self.cleanup_session()
+        await self.app(scope, receive, send)
