a2b6098f93e44c1b988a505baaf16a0128bcab48
Stuart George <stuart@accentdesign.co.uk>
2019-12-04 11:24:09 +0000

    fix one and added another one

--- starlette_core/rubbish.py
+++ starlette_core/rubbish.py
@@ -1,4 +1,9 @@
 import datetime
 
-def bad_input_variable(today=datetime.datetime.now()):
+
+def bad_input_variable(today=None):
+    return today or datetime.datetime.now()
+
+
+def another_bad_input_variable(today=datetime.datetime.now()):
     return today
