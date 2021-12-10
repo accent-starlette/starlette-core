9d0aa694271555a121f117c13582eb24bc338476
Stuart George <stuart@accentdesign.co.uk>
2021-12-10 12:19:37 +0000

    fix linting and depreciation warnings

--- starlette_core/config.py
+++ starlette_core/config.py
@@ -1,5 +1,3 @@
-import typing
-
 from starlette.config import Config
 from starlette.datastructures import CommaSeparatedStrings, Secret
 
@@ -11,20 +9,16 @@ class AppConfig:
     email_backend: str = _config(
         "EMAIL_BACKEND", default="starlette_core.mail.backends.smtp.EmailBackend"
     )
-    email_default_from_address: str = _config("EMAIL_DEFAULT_FROM_ADDRESS", default="")
-    email_default_from_name: str = _config("EMAIL_DEFAULT_FROM_NAME", default="")
-    email_host: str = _config("EMAIL_HOST", default="")
-    email_port: typing.Optional[int] = _config("EMAIL_PORT", cast=int, default=None)
-    email_username: str = _config("EMAIL_USERNAME", default="")
-    email_password: typing.Union[str, Secret] = _config(
-        "EMAIL_PASSWORD", cast=Secret, default=""
-    )
-    email_use_tls: bool = _config("EMAIL_USE_TLS", cast=bool, default=False)
-    email_timeout: typing.Optional[int] = _config(
-        "EMAIL_TIMEOUT", cast=int, default=None
-    )
+    email_default_from_address = _config("EMAIL_DEFAULT_FROM_ADDRESS", default="")
+    email_default_from_name = _config("EMAIL_DEFAULT_FROM_NAME", default="")
+    email_host = _config("EMAIL_HOST", default="")
+    email_port = _config("EMAIL_PORT", cast=int, default=None)
+    email_username = _config("EMAIL_USERNAME", default="")
+    email_password = _config("EMAIL_PASSWORD", cast=Secret, default="")
+    email_use_tls = _config("EMAIL_USE_TLS", cast=bool, default=False)
+    email_timeout = _config("EMAIL_TIMEOUT", cast=int, default=None)
     # templating configuration
-    jinja2_extensions: typing.List[str] = _config(
+    jinja2_extensions = _config(
         "JINJA2_EXTENSIONS", cast=CommaSeparatedStrings, default=[]
     )
 
--- starlette_core/templating.py
+++ starlette_core/templating.py
@@ -19,7 +19,7 @@ class Jinja2Templates(templating.Jinja2Templates):
         self.env = self.get_environment(loader)
 
     def get_environment(self, loader: "jinja2.BaseLoader") -> "jinja2.Environment":
-        @jinja2.contextfunction
+        @jinja2.pass_context
         def get_request_messages(context: dict):
             request = context["request"]
             return get_messages(request)
@@ -27,7 +27,7 @@ class Jinja2Templates(templating.Jinja2Templates):
         def is_multipart(form: form.Form) -> bool:
             return any(isinstance(field, fields.FileField) for field in form)
 
-        @jinja2.contextfunction
+        @jinja2.pass_context
         def url_for(context: dict, name: str, **path_params: typing.Any) -> str:
             request = context["request"]
             return request.url_for(name, **path_params)
