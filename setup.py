88e94be0cc65e457e32f2586a3c8860c6c08fca9
Stuart George <stuart@accentdesign.co.uk>
2022-01-04 16:39:26 +0000

    fix <1.4 error in install

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     packages=get_packages(package_name),
     package_data=get_package_data(package_name),
     install_requires=[
-        'sqlalchemy<1.4',
+        'sqlalchemy~=1.3',
         'sqlalchemy-utils',
         'starlette',
         'typing',
